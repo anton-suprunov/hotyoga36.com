3c37f365feaeb11f433363d782b4e94fce463a5f
Anton Suprunov <suprunov.anton@gmail.com>
2017-04-13 02:50:13 +0500

    home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
 import $ from 'jquery';
 import responsiveNav from 'responsive-nav';
+import slick from 'slick-carousel';
 
 const init = () => {
-  const nav = responsiveNav(".nav", {
-
+  //const nav = responsiveNav(".nav", {});
+  
+  $('.slideshow').slick({
+    prevArrow : null,
+    nextArrow : null,
+    autoplay : true,
+    fade : true,
+    dots : true
   });
 };
 
