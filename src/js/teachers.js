2c057cf0f181ea301e004218dfb31333777e772b
Anton Suprunov <suprunov.anton@gmail.com>
2020-10-29 17:04:49 +0500

    'changes'

--- src/js/teachers.js
+++ src/js/teachers.js
@@ -2,6 +2,10 @@ import $ from 'jquery';
 
 export default function initTeachers() {
   $('.teachers__teacher').on('click', (e) => {
+    const href = $(e.currentTarget).attr('href');
+    //console.log(href, $(this));
+    if (href && href.length && href !== '#') return;
+
     e.preventDefault();
     $('.popup__content').html($(e.currentTarget).html());
     $('.popup').addClass('popup_active');
@@ -10,4 +14,13 @@ export default function initTeachers() {
     $('.popup').removeClass('popup_active');
   });
   
+  $('.teachers__text_short').on('click', function() {
+    $(this).next('.teachers__text_long').addClass('teachers__text_visible');
+    $(this).addClass('teachers__text_hide')
+  });
+
+  $('.teachers__text_long').on('click', function () {
+    $(this).prev('.teachers__text_short').removeClass('teachers__text_hide');
+    $(this).removeClass('teachers__text_visible')
+  })
 };
